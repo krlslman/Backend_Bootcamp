1485a296e3afb0f5d3719202b14f62292258768b
krlslman <selmankorall@gmail.com>
2022-10-07 22:46:45 +0300

    update

---
+++ Django/class-notes/012_Django_Authentications/user_example/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+def home(request):
+    return render(request, 'user_example/index.html')
+
+def special(request):
+    return render(request, "user_example/special.html")
