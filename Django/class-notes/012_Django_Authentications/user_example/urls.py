269ce450ac743f0592dafc8039bf19d4599f5e70
krlslman <selmankorall@gmail.com>
2022-10-08 16:55:15 +0300

    register and login page is added

--- Django/class-notes/012_Django_Authentications/user_example/urls.py
+++ Django/class-notes/012_Django_Authentications/user_example/urls.py
@@ -1,11 +1,23 @@
 from django.urls import path, include
-from .views import (home,special)
-# from django.contrib.auth import views as auth_views
+from .views import (
+    home,
+    special,
+    register
+    )
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('', home, name="home"),
     path('special/', special, name="special"),
-    # path('register/', register, name="register"),
-    # path('password_change/', auth_views.PasswordChangeView.as_view(
-        # template_name="registration/password_change.html"), name="password_change")
+    path('register/', register, name="register"),
+    path('password_change/', auth_views.PasswordChangeView.as_view(
+        template_name="registration/password_change.html"), name="password_change")
 ]
+
+""" 
+4 ÜNCÜ PATHDE NE YAPTIK?
+- password_change adında bir end-point belirledik. 
+- Bu endpoint'e istek geldiğinde beni django tarafından verilen default "auth_views.PasswordChangeView.as_view"üne yönlendir,
+- Ama django tarafından verilen default template yerine "registration/password_change.html" isimli template'imi kullan 
+
+""" 
--- Django/class-notes/012_Django_Authentications/user_example/views.py
+++ Django/class-notes/012_Django_Authentications/user_example/views.py
@@ -1,7 +1,38 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import authenticate, login
 
 def home(request):
     return render(request, 'user_example/index.html')
 
 def special(request):
     return render(request, "user_example/special.html")
+
+def register(request):
+    # form = UserCreationForm()
+    # if request.method == 'POST':
+    #     form = UserCreationForm(request.POST)
+    ## Bu 3 satır yerine aşağıdaki tek satır kod aynı işi yapıyor;
+   
+    #? pass in post data when instantiate the form
+    form = UserCreationForm(request.POST or None)
+    #? if the form is ok with the info filled:
+    if form.is_valid():
+        form.save()
+        #? that creates a new user
+        #? after creation of the user, want to authenticate it
+        username = form.cleaned_data.get("username")
+        password = form.cleaned_data.get("password1")
+        #? inspect the page and see the first password is password1, import authenticate
+        user = authenticate(username=username, password=password)
+        #? want user to login right after registered, import login
+
+        login(request, user)
+        #? want to redirect to home page, import redirect
+        return redirect("login")
+
+    contex = {
+        'form':form
+    }
+
+    return render(request, 'registration/register.html', contex)
