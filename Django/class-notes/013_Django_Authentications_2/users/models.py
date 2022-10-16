de8f0d26e4aa322d6ef3cf84c8c06d4ced901cf5
krlslman <selmankorall@gmail.com>
2022-10-16 14:09:43 +0300

    minor update

--- Django/class-notes/013_Django_Authentications_2/users/models.py
+++ Django/class-notes/013_Django_Authentications_2/users/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class UserProfile(model.Model):
+	portfolio = models.URLField(blank=True)
+	profile_pic = models.ImageFields.(upload_to='profile_pics',blank=True)
+	user = models.OneToOneField(User, on_delete=models.CASCADE)
